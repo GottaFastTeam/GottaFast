62d167f3d351f65fbfe14bee8c91a8d5e33d6f97
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-01-22 02:20:35 +0700

    Done With Dashboard

--- client/src/components/CardUser.vue
+++ client/src/components/CardUser.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="card-group" width='350px'>
-    <div class="card">
-        <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">User 01</h5>
-          <a href="#" class="btn btn-primary">Leave</a>
+  <div class="card-group " width='350px'>
+        <div class="card">
+            <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">User 01</h5>
+              <a href="#" class="btn btn-primary">Leave</a>
+            </div>
         </div>
-    </div>
-    <div class="card">
-      <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
-      <div class="card-body">
-          <h5 class="card-title">User 01</h5>
-      </div>
-    </div>
-    <div class="card">
-      <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">User 01</h5>
-      </div>
-    </div>
+        <div class="card">
+          <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
+          <div class="card-body">
+              <h5 class="card-title">User 01</h5>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">User 01</h5>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/50cff0e0-1005-487c-8705-c04f919c3964" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">User 01</h5>
+          </div>
+        </div>
   </div>
 </template>
 
@@ -32,8 +38,12 @@ export default {
 <style scoped>
   .card {
     margin: 10px;
-    width: 350px;
+    height: 350px;
+    min-width: 100px;
+    max-width: 250px;
     background: #3581b8;
+    border-radius: 10px;
+    margin: auto;
   }
 
   .card-img-top {
