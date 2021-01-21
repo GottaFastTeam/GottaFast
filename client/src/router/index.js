7b8926d512aa97fbc8b25f99dc43999efbc1fa89
Ekomunthe48 <gudepsman.1silaen@gmail.com>
2021-01-21 23:41:15 +0700

    making client dashboard

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import WinnerPage from '../views/WinnerPage.vue'
 import Login from '../views/Login.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
   },
   {
     path: '/about',
