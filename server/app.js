94472c264eb72a3b9dc0f60c706c221ddd507665
here-bossnanda <m.trinandanoviardy@gmail.com>
2021-01-22 14:00:16 +0700

    [nanda] update port to deploy

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // iniital message
 const messages = [{
